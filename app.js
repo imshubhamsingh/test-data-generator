558020fbdd12319a93e8c8348ab57665125f35a4
Shubham Singh <imshubhamsingh97@gmail.com>
2017-06-10 17:12:07 +0530

    changed file structure of generator api

--- app.js
+++ app.js
@@ -9,13 +9,15 @@ const port = process.env.PORT||3010;
 
 app.use(express.static(__dirname+'/public'));
 
-var nameRouter = require('./src/routers/nameRouter')();
-var emailRouter = require('./src/routers/emailRouter')();
-var dateRouter = require('./src/routers/dateRouter')();
+var nameRouter = require('./src/api/humanData/routers/nameRouter')();
+var emailRouter = require('./src/api/humanData/routers/emailRouter')();
+var dateRouter = require('./src/api/humanData/routers/dateRouter')();
 
+//human data
 app.use('/api/names',nameRouter);
 app.use('/api/emails',emailRouter);
 app.use('/api/dates',dateRouter);
+
 
 app.listen(port,function () {
    console.log("Server running on port "+ port);
