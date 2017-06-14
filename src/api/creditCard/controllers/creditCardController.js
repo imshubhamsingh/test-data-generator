11139cbd89e367bb36d8e1f5cc10e7ad37ef4f4e
Shubham Singh <imshubhamsingh97@gmail.com>
2017-06-14 12:04:18 +0530

    Applied Luhn algorithm to check if the credit card number is valid or not

--- src/api/creditCard/controllers/creditCardController.js
+++ src/api/creditCard/controllers/creditCardController.js
@@ -5,39 +5,69 @@
 var creditCardController = function () {
     var creditCardData = require('../../../../data-plugins/cerdit-card/list.json');
 
-    var completed_number = function (prefix, format) {
-        var change,creditCardNo ="";
-        for(var i=0;i<prefix.length;i++){
-            change = 0;
-            if(format.charAt(i) ==="X"){
-                creditCardNo+=prefix.charAt(i);
-                change=1;
-            }
-            if(change === 0){
-                creditCardNo+=format.charAt(i)
-            }
+    function strrev(str) {
+        var revstr='';
+        for (var i = str.length-1; i>=0; i--)
+            if(str.charAt(i)!==" ")revstr+=str.charAt(i);
+        return revstr;
+    }
+//Applying Luhn algorithm to check if the credit card number is valid or not
+    var ccChecker = function (ccNumber) {
+      var checkingCreditCard = strrev(ccNumber);
+      var sum = 0;
+      var pos =0;
+      var even = 0;
+      var odd = 0;
+      while ( pos < checkingCreditCard.length ) {
+          if(pos%2===0){
+                var temp = parseInt(checkingCreditCard.charAt(pos))*2;
+                if(temp>9) temp-=9;
+                even +=temp;
+          }else{
+              odd+= parseInt(checkingCreditCard.charAt(pos));
+          }
+            pos++;
         }
+        sum = even+odd;
+        var checkdigit = (sum*9)%10;
+        return ccNumber+checkdigit.toString();
+    };
 
-        while(creditCardNo.length< format.length-1){
-            change = 0;
-            if(format.charAt(creditCardNo.length-1) ==="X"){
-                creditCardNo+=Math.floor(Math.random()*10).toString();
-                change = 1
-            }
-            if(change === 0){
-                creditCardNo+=format.charAt(creditCardNo.length-1);
-            }
-        }
-        return creditCardNo;
+    var completed_number = function (prefix, format) {
+        var change,creditCardNo="";
+
+                for(var i=0;i<=prefix.length;i++) {
+
+                    change = 0;
+                    if (format.charAt(i) === "X") {
+                        creditCardNo += prefix.charAt(i);
+                        change = 1;
+                    }
+                    if (change === 0) {
+                        creditCardNo += format.charAt(i)
+                    }
+                }
+                while (creditCardNo.length < format.length-1) {
+                    change = 0;
+                    if (format.charAt(creditCardNo.length - 1) === "X") {
+                        creditCardNo += Math.floor(Math.random() * 10).toString();
+                        change = 1
+                    }
+                    if (change === 0) {
+                        creditCardNo += format.charAt(creditCardNo.length - 1);
+
+                    }
+                }
+        return ccChecker(creditCardNo);
     };
 
     var creditCard = function () {
         var index =Math.floor(Math.random()*creditCardData.length);
         var type = creditCardData[index].name;
-        console.log(type);
+        //console.log(type);
         var creditCardNoFormat = creditCardData[index].ccformat[Math.floor(Math.random()*creditCardData[index].ccformat.length)];
         var creditCardNoPrefix = creditCardData[index].ccprefix[Math.floor(Math.random()*creditCardData[index].ccprefix.length)];
-        console.log(creditCardNoPrefix);
+        //console.log(creditCardNoPrefix);
         return {
             "Type":type,
             "creditCardNo":completed_number(creditCardNoPrefix,creditCardNoFormat)
