b5c234c9f53ac8e65d7b57c141917813ed89f605
Shubham Singh <imshubhamsingh97@gmail.com>
2017-06-22 14:06:46 +0530

    tweaked UI

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
+window.$ = window.jQuery = require("jquery");
 require("materialize-css");
 require("materialize-css-file");
 require("custom-scss");
 require("logo");
-console.log($);
 
 
--- public/dist/bundle.js
+++ public/dist/bundle.js
@@ -9888,15 +9888,14 @@ return jQuery;
 /* 1 */
 /***/ (function(module, exports, __webpack_require__) {
 
-/* WEBPACK VAR INJECTION */(function($) {__webpack_require__(3);
+window.$ = window.jQuery = __webpack_require__(0);
+__webpack_require__(3);
 __webpack_require__(6);
 __webpack_require__(7);
 __webpack_require__(8);
-console.log($);
 
 
 
-/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(0)))
 
 /***/ }),
 /* 2 */
