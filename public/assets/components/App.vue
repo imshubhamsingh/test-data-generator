a46164e0dc50eeea41e13c875c1814ae1f34427e
Shubham Singh <imshubhamsingh97@gmail.com>
2017-06-24 19:36:19 +0530

    created name generator UI

--- public/assets/components/App.vue
+++ public/assets/components/App.vue
@@ -3,6 +3,22 @@
         <nav-bar></nav-bar>
         <div class="container">
             <div class="row">
+                <div class="col s6 input-field">
+                    <select>
+                        <option disabled value="" selected>Choose your option</option>
+                        <option value="fullNames">Full Names</option>
+                        <option value="maleNames">Male Names</option>
+                        <option value="femaleNames">Female Names</option>
+                        <option value="surnames">Surname</option>
+                    </select>
+                    <label>Select Name Type</label>
+                </div>
+                <div class="input-field col s6">
+                    <input type="number" v-model="number" min="0">
+                    <label>List size</label>
+                </div>
+            </div>
+            <div class="row col s12">
                 <button class="waves-effect waves-light btn col s12" @click="getSampleData">Get data</button>
             </div>
             <div class="row">
@@ -17,13 +33,22 @@
 </template>
 
 <script>
+    var choice ="";
+    $(document).ready(function() {
+        $('select').material_select();
+        $('select').on('change', function(event) {
+            choice = event.currentTarget.value;
+            console.log(choice);
+        });
+    });
     var axios = require("axios");
     var navBar = require("./Shared/navBar.vue");
     console.log("In App.vue");
     export default{
         data:function(){
-            return{
-                data: '',
+            return {
+                data: "",
+                number: 0
             }
         },
         components:{
@@ -31,10 +56,14 @@
         },
         methods:{
             getSampleData: function () {
+                console.log("Name Type: "+this.selected+" Number: "+this.number );
                 var vm = this;
-                axios.get("https://node-data-generator.herokuapp.com/api/names/fullNames?n=20").then(function (response) {
+                console.log("/api/names/"+choice+"/?n="+vm.number);
+                axios.get("/api/names/"+choice+"/?n="+vm.number).then(function (response) {
+                    console.log(response);
                     // JSON responses are automatically parsed.
                     vm.data = response.data;
+
                 }).catch(function (e) {
                     console.log(e);
                 });
