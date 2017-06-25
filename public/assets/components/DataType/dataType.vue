14784477bd246c5cc87fdeae6a58f440e5627a7b
Shubham Singh <imshubhamsingh97@gmail.com>
2017-06-25 18:38:14 +0530

    created UI for selecting Column Details

--- public/assets/components/DataType/dataType.vue
+++ public/assets/components/DataType/dataType.vue
@@ -1,56 +1,64 @@
 <template>
     <div>
         <div class="container">
-            <!--<div class="fixed-action-btn toolbar">-->
-                <!--<a class="btn-floating btn-large red">-->
-                    <!--<i class="large material-icons">mode_edit</i>-->
-                <!--</a>-->
-                <!--<ul>-->
-                    <!--<li class="waves-effect waves-light"><a href="#!"><i class="material-icons">insert_chart</i></a></li>-->
-                    <!--<li class="waves-effect waves-light"><a href="#!"><i class="material-icons">format_quote</i></a></li>-->
-                    <!--<li class="waves-effect waves-light"><a href="#!"><i class="material-icons">publish</i></a></li>-->
-                    <!--<li class="waves-effect waves-light"><a href="#!"><i class="material-icons">attach_file</i></a></li>-->
-                <!--</ul>-->
-            <!--</div>-->
-            <div class="fixed-action-btn toolbar">
-                <a class="btn-floating btn-large red">
-                    <i class="fa fa-plus large" aria-hidden="true"></i>
-                </a>
-                <ul>
-                    <li class="waves-effect">
-                        <div class="input-field container">
-                            <input id="columnTitle" type="text" class="validate" placeholder="" v-model="columnTitle">
-                            <label for="columnTitle">Column Title</label>
-                        </div>
-                    </li>
-                    <li class="waves-effect">
-                        <div class="input-field container">
-                            <select id="options">
-                                <optgroup label="Human Data">
-                                    <option value="name">Names</option>
-                                    <option value="2">Option 2</option>
-                                </optgroup>
-                                <optgroup label="team 2">
-                                    <option value="3">Option 3</option>
-                                    <option value="4">Option 4</option>
-                                </optgroup>
-                            </select>
-                            <label>Data Type</label>
-                        </div>
-                    </li>
-                    <li class="waves-effect waves-light">
-                        <a @click="addColumn">Add Column</a>
+            <div class="">
+                <div class="fixed-action-btn">
+                    <a class="btn-floating btn-large waves-effect waves-light red large" href="#modal1"><i class="fa fa-plus" aria-hidden="true"></i></a>
+                </div>
+
+                <!-- Modal Structure -->
+                <div id="modal1" class="modal bottom-sheet">
+                    <div class="modal-content">
+                        <h4>Column Details</h4>
+                        <form>
+                            <div class="input-field">
+                                <input id="columnTitle" name="column" type="text" class="validate" placeholder="" v-model="columnName" required aria-required="true">
+                                <label for="columnTitle">Column Title</label>
+                            </div>
+                            <div>
+                                <h6>Human Data</h6>
+                                <div class="row">
+                                    <p class="col s3 m2 l1">
+                                        <input type="radio" value="names" id="names" v-model="columnType"/>
+                                        <label for="names">Names</label>
+                                    </p>
+                                    <p class="col s3 m2 l1">
+                                        <input type="radio" value="date" id="date" v-model="columnType" />
+                                        <label for="date">Date</label>
+                                    </p >
+                                    <p class="col s3 m2 l1">
+                                        <input type="radio" value="email" id="email" v-model="columnType" />
+                                        <label for="email">Email</label>
+                                    </p>
+                                    <p class="col s3 m2 l1">
+                                        <input type="radio" value="phone" id="phone" v-model="columnType" />
+                                        <label for="phone">Phone</label>
+                                    </p>
+                                    <p class="col s3 m2 l1">
+                                        <input type="radio" value="company" id="company" v-model="columnType" />
+                                        <label for="company">Company</label>
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="submit" class="modal-action modal-close waves-effect waves-green btn-flat" name="action" @click="addColumn">Add Column</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <!-- Modal Trigger -->
+            <div v-if="dataTypes.length>0">
+                <ul class="collapsible" data-collapsible="accordion">
+                    <li v-for="(dataType, index) in dataTypes">
+                        <div class="collapsible-header"><a @click="remove($index)"><i class="fa fa-trash" aria-hidden="true"></i></a> {{dataType.columnName}}</div>
+                        <div class="collapsible-body"><name-generator :dataReady="dataGenCall" @nameGenerated="dataGen=$event"></name-generator></div>
                     </li>
                 </ul>
             </div>
 
 
-            <ul class="collapsible" data-collapsible="accordion">
-                <li v-for="dataType in dataTypes">
-                    <div class="collapsible-header">{{dataType}}</div>
-                    <div class="collapsible-body"><name-generator :dataReady="dataGenCall" @nameGenerated="dataGen=$event"></name-generator></div>
-                </li>
-            </ul>
+
         </div>
     </div>
 </template>
@@ -63,17 +71,18 @@
             return {
                 dataGen: [],
                 dataGenCall: this.trigger,
-                dataTypes: ["Name"],
-                columnTitle:"",
-                dataTypeChosen:""
+                dataTypes: [
+                    {
+                        columnName:"Name",
+                        columnType: "name"
+                    }
+                    ],
+                columnName:"",
+                columnType:""
             }
         },
         mounted:function () {
-            $('#options').material_select();
-                $('#options').on('change', function(event){
-                    console.log("hi");
-                    //choice = event.currentTarget.value;
-                });
+            $('.modal').modal();
         },
         props:{
             trigger:{
@@ -93,10 +102,23 @@
                 this.dataGenCall = !this.dataGenCall;
             }
         },
+        computed:{
+
+        },
         methods:{
+            remove: function (index) {
+                this.dataTypes.splice(index,1);
+            },
             addColumn: function () {
-                this.dataTypes.push(this.columnTitle);
-                this.columnTitle ="";
+                if(this.columnName!==""){
+                    this.dataTypes.push({
+                        columnName:this.columnName,
+                        columnType:this.columnType
+                    });
+                }
+
+                this.columnName ="";
+                this.columnType ="";
             }
         }
 
@@ -105,7 +127,12 @@
 
 <style lang="scss" scoped>
     label{
-        color:#fff !important;
+    color:#000 !important;
+    }
+    .modal{
+        &.bottom-sheet{
+            max-height: 60% !important;
+        }
     }
 
 </style>
